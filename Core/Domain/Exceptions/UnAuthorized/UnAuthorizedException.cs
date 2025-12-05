abfb3bff3e1d0a953dfbca86d6dfdf7558bb3cb8
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2025-12-05 23:15:50 +0200

    Added Exception Handling Middlware With UnAuthorized Exception

---
+++ Core/Domain/Exceptions/UnAuthorized/UnAuthorizedException.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Exceptions.UnAuthorized
+{
+    public sealed class UnAuthorizedException : Exception
+    {
+        public UnAuthorizedException(string message = "Invalid  Email or Password") : base(message)
+        {
+        }
+    }
+}
---
+++ TribeUp/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,65 @@
+﻿
+using Domain.Exceptions.UnAuthorized;
+using Shared.ErrorModels;
+
+namespace TribeUp.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+                if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+                    await HandleNotFoundApiAsync(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong ==> : {ex.Message}");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+
+            var response = new ErrorDetails
+            {
+                Message = ex.Message
+            };
+
+            context.Response.StatusCode = ex switch
+            {
+                UnAuthorizedException => StatusCodes.Status401Unauthorized,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            response.StatusCode = context.Response.StatusCode;
+            await context.Response.WriteAsync(response.ToString());
+        }
+
+        private async Task HandleNotFoundApiAsync(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+
+            var response = new ErrorDetails
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                Message = $"The endpoint with url: '{context.Request.Path}' not found"
+            }.ToString();
+
+            await context.Response.WriteAsync(response);
+        }
+    }
+}
