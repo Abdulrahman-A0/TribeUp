31e4586e38a60724a589ef779396f4cc674f0478
Hazem Mofdi <hazemmofdi11@gmail.com>
2025-12-06 16:57:35 +0200

    Created Authentication EndPoints (Login, Register)

---
+++ Infrastructure/Presentation/Controller/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction.Contracts;
+using Shared.DTOs.IdentityModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controller
+{
+    public class AuthenticationController(IServiceManager serviceManager) : ApiController
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserResultDTO>> LoginAsync(LoginDTO loginDTO)
+            => Ok(await serviceManager.AuthenticationService.LoginAsync(loginDTO));
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserResultDTO>> RegisterAsync(RegisterDTO registerDTO)
+            => Ok(await serviceManager.AuthenticationService.RegisterAsync(registerDTO));
+    }
+}
--- Shared/DTOs/IdentityModule/UserResultDTO.cs
+++ Shared/DTOs/IdentityModule/UserResultDTO.cs
@@ -1,4 +1,10 @@
 ﻿namespace Shared.DTOs.IdentityModule
 {
-    public record UserResultDTO(string FullName, string Email, string? ProfilePicture, string Token);
+    public record UserResultDTO()
+    {
+        public string FullName { get; init; }
+        public string Email { get; init; }
+        public string? ProfilePicture { get; init; }
+        public string Token { get; init; }
+    }
 }
