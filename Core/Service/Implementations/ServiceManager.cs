f11a387f43015423b36d1670e2680d5854f5b62a
Abdo Hafez <abdohafez731@gmail.com>
2026-01-16 15:21:11 +0200

    Register Post Service in Service Manager

--- Core/Service/Implementations/ServiceManager.cs
+++ Core/Service/Implementations/ServiceManager.cs
@@ -2,8 +2,13 @@
 
 namespace Service.Implementations
 {
-    public class ServiceManager(Func<IAuthenticationService> _authFactory) : IServiceManager
+    public class ServiceManager(
+        Func<IAuthenticationService> _authFactory,
+        Func<IPostService> _postFactory
+        ) : IServiceManager
     {
         public IAuthenticationService AuthenticationService => _authFactory.Invoke();
+
+        public IPostService PostService => _postFactory.Invoke();
     }
 }
--- Core/ServiceAbstraction/Contracts/IServiceManager.cs
+++ Core/ServiceAbstraction/Contracts/IServiceManager.cs
@@ -1,7 +1,11 @@
-﻿namespace ServiceAbstraction.Contracts
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ServiceAbstraction.Contracts
 {
     public interface IServiceManager
     {
         IAuthenticationService AuthenticationService { get; }
+
+        IPostService PostService { get; }
     }
 }
