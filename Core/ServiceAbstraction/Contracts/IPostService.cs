dc5cf2ca3214cd04e0f531000bafab0b7f4e5cfc
Abdo Hafez <abdohafez731@gmail.com>
2026-01-16 14:03:47 +0200

    Added Post Service Abstraction

---
+++ Core/ServiceAbstraction/Contracts/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.DTOs.Posts;
+namespace ServiceAbstraction.Contracts
+{
+    public interface IPostService
+    {
+        Task CreatePostAsync(CreatePostDTO dto, string userId);
+        Task<IEnumerable<FeedPostDTO>> GetFeedAsync();
+        Task LikePostAsync(int postId, string userId);
+        Task AddCommentAsync(int postId, CreateCommentDTO dto, string userId);
+    }
+}
