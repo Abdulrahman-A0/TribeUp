464e7ca600a0011af836cb63a171464f7632c0e2
Abdo Hafez <abdohafez731@gmail.com>
2026-01-16 13:45:57 +0200

    Added Post Feed Specification

---
+++ Core/Service/Specifications/PostFeedSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.Posts;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Enums;
+
+namespace Service.Specifications
+{
+    internal class PostFeedSpecification : BaseSpecifications<Post,int>
+    {
+        public PostFeedSpecification() 
+            :base(p => p.Accessibility == AccessibilityType.Public)
+        {
+            AddIncludes(p => p.Group);
+            AddIncludes(p => p.Likes);
+            AddIncludes(p => p.Comments);
+        }
+    }
+}
