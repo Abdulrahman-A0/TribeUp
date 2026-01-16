56ade651cceb9b7460fbcdbbeb92e9280971cbb8
Abdo Hafez <abdohafez731@gmail.com>
2026-01-16 13:57:50 +0200

    Added Post & Feed DTOs

---
+++ Shared/DTOs/Posts/CreatePostDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Posts
+{
+    public class CreatePostDTO
+    {
+        public int GroupId { get; set; }
+        public string Caption { get; set; }
+        public string MediaUrl { get; set; }
+    }
+}
---
+++ Shared/DTOs/Posts/FeedPostDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Posts
+{
+    public class FeedPostDTO
+    {
+        public int PostId { get; set; }
+        public string Caption { get; set; }
+        public string MediaUrl { get; set; }
+        public string GroupName { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
