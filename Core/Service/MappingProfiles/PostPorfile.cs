f8adfa084fed908db134f9fdbce5b64b204e3595
Abdo Hafez <abdohafez731@gmail.com>
2026-01-16 14:34:19 +0200

    Added Automapper Profile For Posts

---
+++ Core/Service/MappingProfiles/PostPorfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Entities.Posts;
+using Shared.DTOs.Posts;
+
+namespace Service.MappingProfiles
+{
+    public class PostPorfile : Profile
+    {
+        public PostPorfile()
+        {
+            CreateMap<Post, FeedPostDTO>()
+                .ForMember(dest => dest.PostId, options => options.MapFrom(src => src.Id))
+                .ForMember(dest => dest.GroupName, options => options.MapFrom(src => src.Group.GroupName))
+                .ForMember(dest => dest.LikesCount, options => options.MapFrom(src => src.Comments.Count))
+                .ForMember(dest => dest.CreatedAt, options => options.MapFrom(src => src.CreatedAt));
+
+            CreateMap<CreatePostDTO, Post>();
+        }
+    }
+}
