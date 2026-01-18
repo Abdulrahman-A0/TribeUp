571e3255a557b226ef3b831006aca3d79804958c
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-01-18 21:12:30 +0200

    DTO for old Legacy Login end point

---
+++ Shared/DTOs/IdentityModule/UserResultDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.IdentityModule
+{
+    public record UserResultDTO
+    {
+        public string FullName { get; init; }
+        public string Email { get; init; }
+        public string? ProfilePicture { get; init; }
+        public string? Avatar { get; init; }
+        public string Token { get; init; }
+    }
+}
