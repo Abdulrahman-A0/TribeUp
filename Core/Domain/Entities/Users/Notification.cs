9c4bee32bfc342a4ce9be9b995a3e7b496463371
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-01-24 22:42:43 +0200

    Modified Notification Entity

--- Core/Domain/Entities/Users/Notification.cs
+++ Core/Domain/Entities/Users/Notification.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Shared.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.Users
 {
     public class Notification : BaseEntity<int>
     {
+        public NotificationType Type { get; set; }
+        public string Title { get; set; }
         public string Message { get; set; }
         public bool IsRead { get; set; }
-        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+        public int? ReferenceId { get; set; }
 
         #region Relations
         public string UserId { get; set; }
