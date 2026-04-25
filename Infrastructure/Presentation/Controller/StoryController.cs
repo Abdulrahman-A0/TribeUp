1df2dc1a479af9a842c152d07f698afc266f7b19
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-04-25 15:31:34 +0300

    feat(api): add StoryController with endpoints for story operations

---
+++ Infrastructure/Presentation/Controller/StoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction.Contracts;
+using Shared.DTOs.StoriesModule;
+
+namespace Presentation.Controller
+{
+    [Authorize]
+    public class StoryController(IServiceManager serviceManager) : ApiController
+    {
+        [HttpPost("CreateStory")]
+        public async Task<ActionResult<StoryResponseDTO>> CreateStoryAsync(CreateStoryDTO dto)
+            => Ok(await serviceManager.StoryService.CreateStoryAsync(dto, UserId));
+
+        [HttpDelete("DeleteStory/{storyId}")]
+        public async Task<IActionResult> DeleteStoryAsync(int storyId)
+        {
+            await serviceManager.StoryService.DeleteStoryAsync(storyId, UserId);
+            return NoContent();
+        }
+
+        [HttpGet("GetGroupStories/{groupId}")]
+        public async Task<ActionResult<IEnumerable<StoryResponseDTO>>> GetGroupStoriesAsync(int groupId)
+            => Ok(await serviceManager.StoryService.GetActiveGroupStoriesAsync(groupId, UserId));
+
+        [HttpGet("GetStoryFeed")]
+        public async Task<ActionResult<IEnumerable<StoryFeedItemDTO>>> GetStoryFeedAsync()
+            => Ok(await serviceManager.StoryService.GetStoryFeedAsync(UserId));
+
+        [HttpPut("MarkAsViewed/{storyId}")]
+        public async Task<IActionResult> MarkAsViewedAsync(int storyId)
+        {
+            await serviceManager.StoryService.MarkStoryAsViewedAsync(storyId, UserId);
+            return NoContent();
+        }
+    }
+}
