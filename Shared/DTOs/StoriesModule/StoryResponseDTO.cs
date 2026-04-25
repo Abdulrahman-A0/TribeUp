d7542bdc451d2aa3538edc2fef4c829c97e73ef2
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-04-25 15:30:14 +0300

    4. feat(dto): add story DTOs

---
+++ Shared/DTOs/StoriesModule/StoryResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.DTOs.StoriesModule
+{
+    public record StoryResponseDTO
+    {
+        public int Id { get; init; }
+        public string? Caption { get; init; }
+        public string? MediaURL { get; init; }
+        public DateTime CreatedAt { get; init; }
+        public DateTime? ExpiresAt { get; init; }
+        public int ViewsCount { get; init; }
+
+        // Creator info
+        public string CreatorId { get; init; }
+        public string CreatorUserName { get; init; }
+        public string? CreatorProfilePicture { get; init; }
+
+        public bool IsViewedByCurrentUser { get; init; }
+    }
+}
