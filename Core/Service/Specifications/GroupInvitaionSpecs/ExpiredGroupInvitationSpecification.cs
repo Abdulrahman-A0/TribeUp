b745099dfcfe7903453a5f1126ccf9c434440354
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-04-30 16:56:49 +0300

    Replace invitation retrieval spec with expired filter
    Removed GetGroupInvitationsSpecification and added ExpiredGroupInvitationSpecification to filter group invitations that are expired, not revoked, and match the given group and user. This refocuses the logic on handling expired invitations rather than general retrieval.

---
+++ Core/Service/Specifications/GroupInvitaionSpecs/ExpiredGroupInvitationSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Groups;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specifications.GroupInvitaionSpecs
+{
+    public class ExpiredGroupInvitationSpecification : BaseSpecifications<GroupInvitation, int>
+    {
+        public ExpiredGroupInvitationSpecification(int groupId, string userId)
+            : base(i => i.GroupId == groupId &&
+                        i.UserId == userId &&
+                        !i.IsRevoked &&
+                        i.ExpiresAt != null && i.ExpiresAt <= DateTime.UtcNow)
+        {
+        }
+    }
+}
