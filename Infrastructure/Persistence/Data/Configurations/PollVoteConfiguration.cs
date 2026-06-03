669df35a32a54f2aba2994e3182f76a0e4002831
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-06-03 22:55:56 +0300

    Add configurations for Poll and PollVote entities
    Added `PollConfiguration` to define the `Poll` entity's relationships, including a `Restrict` delete behavior for the `CreatedByUserId` foreign key.
    
    Added `PollVoteConfiguration` to define the `PollVote` entity's relationships:
    - Unique index on `OptionId` and `UserId`.
    - `NoAction` delete behavior for the `PollId` foreign key.
    - `Cascade` delete behavior for the `OptionId` and `UserId` foreign keys.

---
+++ Infrastructure/Persistence/Data/Configurations/PollVoteConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities.Engagement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    public class PollVoteConfiguration : IEntityTypeConfiguration<PollVote>
+    {
+        public void Configure(EntityTypeBuilder<PollVote> builder)
+        {
+            builder.HasIndex(pv => new { pv.OptionId, pv.UserId }).IsUnique();
+
+            builder.HasOne(pv => pv.Poll)
+                .WithMany()
+                .HasForeignKey(pv => pv.PollId)
+                .OnDelete(DeleteBehavior.NoAction); 
+
+            builder.HasOne(pv => pv.PollOption)
+                .WithMany(o => o.PollVotes)
+                .HasForeignKey(pv => pv.OptionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(pv => pv.User)
+                .WithMany()
+                .HasForeignKey(pv => pv.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
