a781fa808e7ee1493bea2da2ffbc37978c840f10
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-06-03 22:55:28 +0300

    Add specifications for filtering and retrieving polls
    Added `GroupPollsCountSpecification` to filter polls by group ID.
    Added `GroupPollsSpecification` for paginated group polls with related entities and sorting.
    Added `PollFullDetailsSpecification` to retrieve detailed poll data, including options, votes, and users.
    Added `PollWithOptionsSpecification` to fetch polls with their options.
    Added `UserPollVotesSpecification` to filter votes by poll ID and user ID.

---
+++ Core/Service/Specifications/PollSpecs/GroupPollsCountSpecification.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Engagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specifications.PollSpecs
+{
+    public class GroupPollsCountSpecification : BaseSpecifications<Poll, int>
+    {
+        public GroupPollsCountSpecification(int groupId)
+            : base(p => p.GroupId == groupId)
+        {
+        }
+    }
+}
