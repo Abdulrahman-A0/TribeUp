0dd28763eb469542e620fcc3d97c476d0a2e4891
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-06-06 06:46:36 +0300

    Add specs and DTOs for group message handling
    Added `GroupChatMessageByIdSpec` to retrieve non-deleted
    group messages by ID. Introduced `EditGroupMessageDTO`
    for updating group message content. Added
    `EditedMessageResponseDTO` to structure the response
    after editing a group message.

---
+++ Core/Service/Specifications/GroupChatMessageSpecs/GroupChatMessageByIdSpec.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Groups;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Specifications.GroupChatMessageSpecs
+{
+    public class GroupChatMessageByIdSpec : BaseSpecifications<GroupChatMessage, long>
+    {
+        public GroupChatMessageByIdSpec(long messageId)
+            : base(m => m.Id == messageId && !m.IsDeleted)
+        {
+        }
+    }
+}
---
+++ Shared/DTOs/GroupMessageModule/EditGroupMessageDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.GroupMessageModule
+{
+    public class EditGroupMessageDTO
+    {
+        public string Content { get; set; } = string.Empty;
+    }
+}
